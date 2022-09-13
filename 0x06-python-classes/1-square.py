97add5671c101abfc7f932b8c74668be5abec72a
iphilopy <105018620+iphilopy@users.noreply.github.com>
2022-09-13 14:25:59 -0700

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
+"""a script that runs a class"""
 #!/usr/bin/python3
 class Square:
+    """a class Square that defines a square"""
     def __init__(self, size):
         self.__size = size
+        """sizes"""
